766f120b14a7fd54ded829663be2feda821bdeb8
Gian Carnero <gian.carnero@gmail.com>
2022-06-13 12:33:16 -0500

    6.13.22 Challenge

--- 6.13.22/6.13.22.js
+++ 6.13.22/6.13.22.js
@@ -7,7 +7,7 @@
 // cap is the amount of people the bus can hold excluding the driver.
 // on is the number of people on the bus excluding the driver.
 // wait is the number of people waiting to get on to the bus excluding the driver.
-// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take. 
 
 function enough(cap, on, wait) {
     if (on + wait < cap){
