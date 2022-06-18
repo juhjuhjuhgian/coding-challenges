8bc9eefb075c63d7a5023a718313af400973e620
Gian Carnero <gian.carnero@gmail.com>
2022-06-18 13:03:15 -0500

    6/18/22 Challenge

--- 6.17.22/6.17.22.js
+++ 6.17.22/6.17.22.js
@@ -12,7 +12,7 @@
 
 function strCount(str, letter){  
     let count = 0;
-    for (let i = 0; i < str.length; i++) {
+    for(let i = 0; i < str.length; i++){
       if (str[i] == letter)
         count++;
     }
