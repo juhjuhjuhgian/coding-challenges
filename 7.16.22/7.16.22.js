8aaacceb784dc09ddd5229b7281802887cf2d49c
Gian Carnero <gian.carnero@gmail.com>
2022-07-16 12:35:28 -0500

    7.16.22. Challenge

---
+++ 7.16.22/7.16.22.js
@@ -0,0 +1,11 @@
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year){
+    return Math.ceil(year / 100);
+}
