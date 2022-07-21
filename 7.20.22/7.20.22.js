6bf3a4520c15dc872c6c4097313798465ac148f3
Gian Carnero <gian.carnero@gmail.com>
2022-07-20 20:04:16 -0500

    7.20.22 Challenge

---
+++ 7.20.22/7.20.22.js
@@ -0,0 +1,14 @@
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+function findNeedle(haystack) {
+    let location = haystack.indexOf('needle')
+      return `found the needle at position ${location}`
+    }
