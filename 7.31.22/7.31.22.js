6e178e35b34916020b4cd316b04b487d6773351a
Gian Carnero <gian.carnero@gmail.com>
2022-07-31 11:48:05 -0500

    7.31.22 Challenges

---
+++ 7.31.22/7.31.22.js
@@ -0,0 +1,16 @@
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+// Return True if yes, False otherwise :)
+
+return (bullets / 2 >= dragons) ? true : false;
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    let arr = [];
+      for (let i=n; i>0; i--) {
+        arr.push(i);
+        } return arr;
+    };
