0ebf532d76afe2f7c93d906a2cf468dd97bcb460
Gian Carnero <gian.carnero@gmail.com>
2022-08-21 19:57:48 -0500

    8.21.22 Challenge

---
+++ 8.2022/8.21.22.js
@@ -0,0 +1,25 @@
+// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+
+//P the parameters being passed in in this function are two arrays, only integers are included 
+//R the result should be both arrays combined and in order from smallest integer to largest
+//E mergeArrays([1, 5, 6, 8], [2, 4, 7, 8]) would return [1,2,4,5,6,7,8]
+//P I'm going to use the first parameter and filter that to make sure it doesn't include any of the same integers in arr2
+  //I'm then going to combine them using the concat method
+  //I'm then going to sort them using the sort method, passing in the first number and subsequent numbers. 
+  function mergeArrays(arr1, arr2) {
+    return arr1
+      .filter((item) => !arr2.includes(item))
+      .concat(arr2)
+      .sort((a, b)=> a-b)
+  }
