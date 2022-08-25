5d13d69a432ca702cd93677a2c2f0bb7885cd35e
Gian Carnero <gian.carnero@gmail.com>
2022-08-25 13:44:17 -0500

    Another Challege completed

--- 8.2022/8.25.22.js
+++ 8.2022/8.25.22.js
@@ -18,3 +18,20 @@ websites.push('codewars')
 function arrayMadness(a, b) {
     return a.reduce((a, b)=> a + b**2, 0) > b.reduce((a, b) => a + b**3, 0) ? true : false
   }
+
+//   Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+//   Example
+//   [2, 1, 10]  -->  9
+//   In descending order: [10, 2, 1]
+  
+//   Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+  function sumOfDifferences(arr) {
+    let sorted = arr.sort((a, b) => b - a)
+    let sum = 0;
+    for (let i = 0; i < sorted.length - 1; i++) {
+      sum += (arr[i] - arr[i + 1]);
+    }
+    return sum;
+  }
