481b566a8c5a1e0446d096c9c60a9f2721f1f1de
Gian Carnero <gian.carnero@gmail.com>
2022-09-12 16:55:18 -0500

    Second Challenge

--- 9.2022/9.12.22.js
+++ 9.2022/9.12.22.js
@@ -6,3 +6,20 @@
 // * "1234!_ "     -> "11223344!!__  "
 
 const doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
