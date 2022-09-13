9acdb3e3e87282d62d9128e832ca08aae74c9e19
Gian Carnero <gian.carnero@gmail.com>
2022-09-13 11:50:41 -0500

    Second Challenge

--- 9.2022/9.13.22.js
+++ 9.2022/9.13.22.js
@@ -14,3 +14,25 @@ function getCount(str) {
     })
      return sum
    }
+
+//    Given a list of integers, determine whether the sum of its elements is odd or even.
+
+//    Give your answer as a string matching "odd" or "even".
+   
+//    If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+    let total = array.reduce((a, b)=> a + b, 0)
+    
+    if(array === []){
+      return [0]
+    } else if (total % 2 === 0){
+      return 'even'
+    } else {
+      return 'odd'
+    }
+ }
+
+ function oddOrEven(arr) {
+    return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+  }
