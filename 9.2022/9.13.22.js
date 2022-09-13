63cb717212a6e8a9eccc3f3a772f641218409c2f
Gian Carnero <gian.carnero@gmail.com>
2022-09-13 11:43:49 -0500

    9.13.22 Challenge

---
+++ 9.2022/9.13.22.js
@@ -0,0 +1,16 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let sum = 0
+    let split = str.split('')
+     
+    split.forEach((item, index)=> {
+        vowels.includes(item) ? sum++ : sum
+    })
+     return sum
+   }
