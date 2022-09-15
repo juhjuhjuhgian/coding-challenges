7e55817d30a5c9678c0c828bb154e7bc51683f2b
Gian Carnero <gian.carnero@gmail.com>
2022-09-15 13:48:58 -0500

    Second Challenge

--- 9.2022/9.15.22.js
+++ 9.2022/9.15.22.js
@@ -10,3 +10,17 @@ function feast(beast, dish) {
     
     return (bSplit[0] === dSplit[0]) && (bSplit[bSplit.length -1] === dSplit[dSplit.length -1]) ? true : false
     }
+
+    // Factors are numbers you can multiply together to get another number.
+
+    // 2 and 3 are factors of 6 because: 2 * 3 = 6
+    
+    // You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+    // You can use the mod operator (%) in most languages to check for a remainder
+    // For example 2 is not a factor of 7 because: 7 % 2 = 1
+    
+    // Note: base is a non-negative number, factor is a positive number.
+
+    function checkForFactor (base, factor) {
+        return base % factor === 0;
+      }
