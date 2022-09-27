b6056d745e44045e9c39ace059610fcc9ada7050
Gian Carnero <gian.carnero@gmail.com>
2022-09-27 16:04:04 -0500

    Third Challenge

--- 9.2022/9.27.22.js
+++ 9.2022/9.27.22.js
@@ -26,3 +26,11 @@ function mouthSize(animal) {
     }
     return arr;
   }
+
+//   Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+function unusualFive() {
+  return 'gianc'.length
+}
