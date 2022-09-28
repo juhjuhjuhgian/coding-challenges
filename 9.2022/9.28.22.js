2a6d3ef8daa483dc01adf7726bcdc731b84bd61a
Gian Carnero <gian.carnero@gmail.com>
2022-09-28 13:38:35 -0500

    Second Challenge

--- 9.2022/9.28.22.js
+++ 9.2022/9.28.22.js
@@ -36,3 +36,24 @@ function getDrinkByProfession(param){
           return "Beer"
     }
   }
+
+//   Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+//   Examples (input -> output)
+//   '! !'                 -> '! !'
+//   '123456789'           -> ''
+//   'This looks5 grea8t!' -> 'This looks great!'
+//   Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+
+  //p a string is being passed in
+//r the same string without any numbers
+//e stringClean('hell0o') would return 'hello'
+//p i used the split method, targeting each character
+  //then i filtered out any character that was not a number by using the filter method and includes method with a bang symbol
+  //join method
+
+  
+function stringClean(s){
+    return s.split('').filter(x => !'1234567890'.includes(x)).join('')
+  }
