dfe5008a80842ab38a38d5397764984e99cc7c9c
Gian Carnero <gian.carnero@gmail.com>
2022-09-28 13:41:48 -0500

    Third Challenge

--- 9.2022/9.28.22.js
+++ 9.2022/9.28.22.js
@@ -57,3 +57,16 @@ function getDrinkByProfession(param){
 function stringClean(s){
     return s.split('').filter(x => !'1234567890'.includes(x)).join('')
   }
+
+//   You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+//   As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+  
+//   Example (Input --> Output)
+  
+//   "Hello World" --> "World Hello"
+//   "Hi There." --> "There. Hi"
+
+function reverse(string){
+    return string.split(' ').reverse().join(' ');
+  }
