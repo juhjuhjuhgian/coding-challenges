f7f956a0270ffaa777bc22f37c3619d162528f6f
Gian Carnero <gian.carnero@gmail.com>
2022-09-28 15:48:34 -0500

    Third Challenge

--- 9.2022/9.28.22.js
+++ 9.2022/9.28.22.js
@@ -70,3 +70,16 @@ function stringClean(s){
 function reverse(string){
     return string.split(' ').reverse().join(' ');
   }
+
+//   Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+// Example:
+
+// sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
+
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+  function sayHello( name, city, state ) {
+    let x= name.toString().split(',').join(' ')
+    return `Hello, ${x}! Welcome to ${city}, ${state}!`
+  }
