e0434f22b289ec1164bb8622bde28267c6a3bcd8
Gian Carnero <gian.carnero@gmail.com>
2022-11-08 20:30:41 -0600

    11.8.22 Challenge

---
+++ 11.2022/11.8.22.js
@@ -0,0 +1,13 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+//problem
+
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+
+//solved
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
