536961a4c96c223cad1928a216dd049423935bc2
Gian Carnero <gian.carnero@gmail.com>
2022-11-18 15:17:23 -0600

    11.18.22 Challenge

---
+++ 11.2022/11.18.22.js
@@ -0,0 +1,17 @@
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+    let z = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+   return str.split('').filter(item => z.includes(item)).reverse().join('')
+  }
