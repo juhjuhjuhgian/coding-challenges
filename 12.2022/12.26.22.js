fc0565c2711534cda982fa39641b83aa352e109d
Gian Carnero <gian.carnero@gmail.com>
2022-12-26 12:26:34 -0600

    Second Challenge

--- 12.2022/12.26.22.js
+++ 12.2022/12.26.22.js
@@ -26,3 +26,42 @@ function correctTail(bod, tail){
        return false
     }
    }
+
+//    Debug celsius converter
+// Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius. Unfortunately his code has some bugs.
+
+// Find the errors in the code to get the celsius converter working properly.
+
+// To convert fahrenheit to celsius:
+
+// celsius = (fahrenheit - 32) * (5/9)
+// Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
+
+//before
+
+// function weatherInfo (temp) {
+//    var c : convert(temp)
+//    if (c > 0)
+//      return (c + " is freezing temperature")
+//    else
+//      return (c + " is above freezing temperature")
+//  }
+ 
+//  function convertToCelsius (temperature) {
+//    var celsius = (tempertur) - 32 + (5/9)
+//    return temperature
+//  }
+
+//after
+function weatherInfo (temp) {
+   var c = convertToCelsius(temp);
+   if (c <= 0)
+     return (c + " is freezing temperature");
+   else
+     return (c + " is above freezing temperature");
+ }
+ 
+ function convertToCelsius (temperature) {
+   var celsius = (temperature - 32) * (5/9);
+   return celsius;
+ }
