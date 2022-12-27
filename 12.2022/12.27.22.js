8be69348f158c34d02f9317c503a8e728138147b
Gian Carnero <gian.carnero@gmail.com>
2022-12-27 12:47:01 -0600

    12.27.22 Challenge

---
+++ 12.2022/12.27.22.js
@@ -0,0 +1,14 @@
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+//before
+// var a == "dev"
+// var b == "Lab"
+
+// var name == a + b
+
+//after
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
