0b29db8f362617407d2a922373b86e29151ab789
Gian Carnero <gian.carnero@gmail.com>
2023-01-06 20:37:45 -0600

    1.6.23 Challenge

---
+++ 1.2023/1.6.23.js
@@ -0,0 +1,14 @@
+// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi!"
+// remove("Hi!!!") === "Hi!"
+// remove("!Hi") === "Hi!"
+// remove("!Hi!") === "Hi!"
+// remove("Hi! Hi!") === "Hi Hi!"
+// remove("Hi") === "Hi!"
+
+function remove (string) {
+    let s = string.split('').filter(item => item !== '!').join('')
+    return `${s}!`
+  }
