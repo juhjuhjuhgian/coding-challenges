cfc47648eea408fe57bb14152c0989db6cf8de27
Gian Carnero <gian.carnero@gmail.com>
2023-01-10 12:28:04 -0600

    Third Challenge

--- 1.2023/1.10.23.js
+++ 1.2023/1.10.23.js
@@ -23,3 +23,16 @@ function combineNames(a, b){
   function isLockNessMonster(s) {
     return s.includes('3.50') || s.includes('three fifty') || s.includes('tree fiddy') ? true : false
   }
+
+//   Sometimes, I want to quickly be able to convert miles per imperial gallon into kilometers per liter.
+
+// Create an application that will display the number of kilometers per liter (output) based on the number of miles per imperial gallon (input).
+
+// Make sure to round off the result to two decimal points.
+
+// Some useful associations relevant to this kata: 1 Imperial Gallon = 4.54609188 litres 1 Mile = 1.609344 kilometres
+
+
+  function converter (mpg) {
+    return +(mpg*1.609344/4.54609188).toFixed(2)
+  }
