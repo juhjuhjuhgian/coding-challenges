a4e775955496e70a4d00a29e173744077f0261df
Gian Carnero <gian.carnero@gmail.com>
2023-01-14 13:40:06 -0600

    Second Challenge

--- 1.2023/1.14.23.js
+++ 1.2023/1.14.23.js
@@ -11,3 +11,13 @@ class Solution{
       console.log("Hello World!");
     }  
   }
+
+//   Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+function trueOrFalse(val){
+    return val == false || val == 0 || val == -0 || val == null || val == undefined || val == NaN ? "false" : "true"
+  }
