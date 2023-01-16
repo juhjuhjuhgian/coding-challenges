a633d26eb00cdd9e0ddf6057cfdac7511d8c93d2
Gian Carnero <gian.carnero@gmail.com>
2023-01-15 19:34:57 -0600

    Second Challenge

--- 1.2023/1.15.23.js
+++ 1.2023/1.15.23.js
@@ -19,3 +19,12 @@ function leo(oscar){
     return "Leo got one already!" 
     } 
 }
+
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+function odds(values){
+  return values.filter( item => item % 2 !== 0);
+}
