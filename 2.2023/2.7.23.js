429ce8543a5ecc752f8484bb665a909d7520b15b
Gian Carnero <gian.carnero@gmail.com>
2023-02-07 11:15:39 -0600

    2.7.23 Challenge

---
+++ 2.2023/2.7.23.js
@@ -0,0 +1,10 @@
+// Task:
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+// 42, "number"   --> true
+// "42", "number" --> false
+
+function typeValidation(variable, type) {
+    return typeof variable === type 
+  }
