069dbdd8b007ec364cda39f6673b541adf77083a
Gian Carnero <gian.carnero@gmail.com>
2023-03-02 13:33:37 -0600

    3.2.23 Challenge

---
+++ 3.2023/3.2.23.js
@@ -0,0 +1,7 @@
+// Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+
+class Cat extends Animal {
+  speak(){
+    return `${this.name} meows.`
+  }
+}
