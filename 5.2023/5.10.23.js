de8347fc7d573c15234e7717b16946f7864d2ed1
Gian Carnero <gian.carnero@gmail.com>
2023-05-10 13:12:12 -0500

    5.10.23 Challenge

---
+++ 5.2023/5.10.23.js
@@ -0,0 +1,13 @@
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+
+function maxDiff(list) {
+    let x = list.sort((a, b) => a - b)
+    
+    return x.length < 2 ? 0 : x[x.length -1] - x[0]
+  };
