2ec8fba3c768ad1cf4215552cd57d9d16e497620
Gian Carnero <gian.carnero@gmail.com>
2023-05-14 14:20:38 -0500

    5.14.23 Challenge

---
+++ 5.2023/5.14.23.js
@@ -0,0 +1,16 @@
+// Task
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+
+
+function maxProduct(numbers, size){
+    return numbers.sort((a, b) => b - a).slice(0,size).reduce((a, b) => a * b)
+     }
+   
