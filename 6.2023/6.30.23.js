e811fe866ef1ae498f174288ab69e0c2658d4734
Gian Carnero <gian.carnero@gmail.com>
2023-06-30 08:26:46 -0500

    Second Challenge

--- 6.2023/6.30.23.js
+++ 6.2023/6.30.23.js
@@ -20,3 +20,41 @@
 function getNames(data){
     return data.map(item => item.name);
   } 
+
+//SECOND CHALLENGE
+
+// The code provided is supposed return a person's Full Name given their first and last names.
+
+// But it's not working properly.
+
+// Notes
+// The first and/or last names are never null, but may be empty.
+
+// Task
+// Fix the bug so we can all go home early.
+
+//BEFORE
+
+// class Dinglemouse{
+
+//     constructor( firstName, lastName ){
+//     }
+    
+//     getFullName(){
+//       return firstName+" "+lastName
+//     }
+    
+//   }
+
+//AFTER
+
+// class Dinglemouse{
+//     constructor( firstName, lastName ){
+//       this.firstName = firstName
+//       this.lastName = lastName
+//     }
+    
+//     getFullName(){
+//       return (`${this.firstName} ${this.lastName}`).trim()
+//     }
+//   }
