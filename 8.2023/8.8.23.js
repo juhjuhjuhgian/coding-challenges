d4ed4c945370397c0220c607a8723292c0198add
Gian Carnero <gian.carnero@gmail.com>
2023-08-08 09:11:01 -0500

    8.8.23.js

---
+++ 8.2023/8.8.23.js
@@ -0,0 +1,10 @@
+// Write function which will create a string from a list of strings, separated by space.
+
+// Example:
+
+// ["hello", "world"] -> "hello world"
+
+//this one was a gimmie.
+function wordsToSentence(words) {
+    return words.join(' ')
+  }
